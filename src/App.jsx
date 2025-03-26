ba30fb095198fe487951bf7e6b293d4454a16e04
Ali Samadi <alisamadi.096@gmail.com>
2025-03-25 20:59:43 -0500

    write code for App.jsx in a static mode and write function inside the App.jsx, still not working properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,71 @@
+import { useEffect } from "react";
+import { useState } from "react";
+const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/";
+const COHORT = "/2412-DEMO";
+const RESOURCE = "/guests";
+const API = BASE_URL + COHORT + RESOURCE;
+
 export default function App() {
-  return <></>;
+  const [guests, setGuests] = useState([
+    {
+      id: 1,
+      name: "Ali Samadi",
+      Email: "alisamadi.096@gmail.com",
+      phone: "+18168594669",
+      bio: "ceo-business",
+      job: "web-developer",
+    },
+  ]);
+
+  const getGuests = async () => {
+    try {
+      const response = await fetch(API);
+      if (!response.ok) throw Error(":(");
+      const result = await response.json();
+      setGuests(result.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getGuests();
+  });
+  return (
+    <>
+      <h1>Guest List</h1>
+      <GuestList guests={guests} />
+    </>
+  );
+}
+
+function GuestList({ guests }) {
+  return (
+    <ul className="guests">
+      {guests.map((guest) => (
+        <GuestCard key={guest.id} guest={guest} onClick={() => GuestDetail()} />
+      ))}
+    </ul>
+  );
+}
+
+function GuestCard({ guest }) {
+  return (
+    <li className="guest">
+      <h2>{guest.name}</h2>
+      <p>{guest.Email}</p>
+      <p>{guest.phone}</p>
+    </li>
+  );
+}
+function GuestDetail({ guest }) {
+  return (
+    <div className="guest details">
+      <h2>{guest.name}</h2>
+      <p>{guest.Email}</p>
+      <p>{guest.phone}</p>
+      <p>{guest.bio}</p>
+      <p>{guest.job}</p>
+    </div>
+  );
 }
